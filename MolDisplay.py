bc30ad1960a5959973406ff569a428674a1861f2
Eric Buys <ebuys@uoguelph.ca>
2023-02-20 13:04:06 -0500

    Completed Part3

--- MolDisplay.py
+++ MolDisplay.py
@@ -46,16 +46,16 @@ class Bond:
         self.z = bond.z
     
     def __str__(self):
-        return """%d %d %d %f %f %f %f %f %f %f""" % (self.bond.a1, self.bond.a2, self.bond.epairs, self.bond.x1, self.bond.y1, self.bond.x2, self.bond.y2, self.bond.len, self.bond.dx, self.bond.dy)
+        return """%d: %d %d %d %f %f %f %f %f %f %f""" % (self.z, self.bond.a1, self.bond.a2, self.bond.epairs, self.bond.x1, self.bond.y1, self.bond.x2, self.bond.y2, self.bond.len, self.bond.dx, self.bond.dy)
 
     def svg(self):
-        x11 = xCoordSVG(self.bond.x1) - self.bond.dy*10
+        x11 = xCoordSVG(self.bond.x1) + self.bond.dy*10
         y11 = yCoordSVG(self.bond.y1) - self.bond.dx*10
-        x12 = xCoordSVG(self.bond.x1) + self.bond.dy*10
+        x12 = xCoordSVG(self.bond.x1) - self.bond.dy*10
         y12 = yCoordSVG(self.bond.y1) + self.bond.dx*10
-        x21 = xCoordSVG(self.bond.x2) - self.bond.dy*10
+        x21 = xCoordSVG(self.bond.x2) + self.bond.dy*10
         y21 = yCoordSVG(self.bond.y2) - self.bond.dx*10
-        x22 = xCoordSVG(self.bond.x2) + self.bond.dy*10
+        x22 = xCoordSVG(self.bond.x2) - self.bond.dy*10
         y22 = yCoordSVG(self.bond.y2) + self.bond.dx*10
 
         return '  <polygon points="%.2f,%.2f %.2f,%.2f %.2f,%.2f %.2f,%.2f" fill="green"/>\n' % (x11, y11, x12, y12, x22, y22, x21, y21)
@@ -64,18 +64,62 @@ class Molecule(molecule.molecule):
     def __str__(self):
         returnStr = ""
         for i in range(self.atom_no):
-            returnStr += "Atom " + i + ": " + Atom(self.get_atom(i)) + "\n";
+            returnStr += "Atom %d: %s\n" % (i, Atom(self.get_atom(i)))
         for i in range(self.bond_no):
-            returnStr += "Bond " + i + ": " + Bond(self.get_bond(i)) + "\n";
+            returnStr += "Bond %d: %s\n" % (i, Bond(self.get_bond(i)))
         return returnStr
+    
+    def svg(self):
+        tempList = [header]
+
+        i = 0
+        j = 0
+        while(i < self.atom_no and j < self.bond_no):
+            a1 = Atom(self.get_atom(i))
+            b1 = Bond(self.get_bond(j))
+            if a1.z < b1.z:
+                tempList.append(a1.svg())
+                i += 1
+            else: # b1.z < a1.z
+                tempList.append(b1.svg())
+                j += 1
+
+        while(i < self.atom_no):
+            a1 = Atom(self.get_atom(i))
+            tempList.append(a1.svg())
+            i += 1
+        while(j < self.bond_no):
+            b1 = Bond(self.get_bond(j))
+            tempList.append(b1.svg())
+            j += 1
+
+        
+        tempList.append(footer)
+        return "".join(tempList)
+    
+    def parse(self, file):
+        fileContents = file.read().split("\n")
+        molCounts = fileContents[3].split()
+        atomNum = int(molCounts[0])
+        bondNum = int(molCounts[1])
+
+        for i in range(atomNum):
+            atomContents = fileContents[4 + i].split()
+            print(atomContents)
+            self.append_atom(atomContents[3], float(atomContents[0]), float(atomContents[1]), float(atomContents[2]))
+        for i in range(bondNum):
+            bondContents = fileContents[4 + atomNum + i].split()
+            print(bondContents)
+            self.append_bond(int(bondContents[0]), int(bondContents[1]), int(bondContents[2]))
+
+        file.close()
 
 
 if __name__ == "__main__":
-    tempA1 = molecule.atom('H', 1, 2, 3)
-    a1 = Atom(tempA1)
-    print(a1)
-    tempList = []
-    tempList.append("hellooooooo")
-    tempList.append("joel is kind of a cool person")
-    print("".join(tempList))
-
+    file = open("caffeine-3D-structure-CT1001987571.sdf", "r")
+    mol = Molecule()
+    mol.parse(file)
+    molecule.molsort(mol)
+    file = open("test.svg", "w")
+    file.write(mol.svg())
+    file.close()
